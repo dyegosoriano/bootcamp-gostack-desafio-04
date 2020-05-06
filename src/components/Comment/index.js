edf5481735587afc0c67ad006e7e00f7a0529399
Dyego Soriano <dyegodf2@gmail.com>
2020-05-06 15:53:34 -0300

    coments

---
+++ src/components/Comment/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './styles.css'
+
+function Comment ({ comment }) {
+  return (
+    <div className='comment'>
+      <img src={comment.author.avatar}/>
+      <p><strong>{comment.author.name}</strong> {comment.content}</p>
+    </div>
+  )
+}
+
+export default Comment
